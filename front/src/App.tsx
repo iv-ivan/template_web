95bac4ec003c8f334a167a1c7f420da47599373a
iv-ivan <iv-ivan@yandex-team.ru>
2021-09-14 23:35:58 +0300

    routing

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,14 +2,43 @@ import React from 'react'
 import { hot } from 'react-hot-loader/root'
 import { MyJSComponent } from './components/MyJSComponent'
 import { Counter } from './components/Counter'
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect
+} from "react-router-dom"
 
-export const App = hot(_App)
+const Home = () => {
+    return (
+        <h1>Hello home</h1>
+    );
+}
+
+const Appointments = () => {
+    return (
+        <h1>Appointments!</h1>
+    );
+}
+
+export const App = hot(_App);
+
 export function _App(): JSX.Element | null {
     return (
-        <div>
-            <h1>Hello world!</h1>
-            <MyJSComponent />
-            <Counter />
-        </div>
+        <Router>
+            <div>
+                <Switch>
+                    <Route path='/home'>
+                        <Home />
+                    </Route>
+                    <Route path='/appointments'>
+                        <Appointments />
+                    </Route>
+                    <Redirect from='/' to='/home'/>
+                  </Switch>
+                <MyJSComponent />
+                <Counter />
+            </div>
+        </Router>
     )
 }
